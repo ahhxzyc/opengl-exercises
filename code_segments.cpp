f77a69453285215e9c8bf692b26db98306a42f2e
ahhxzyc <47681537+ahhxzyc@users.noreply.github.com>
2020-11-19 23:30:01 +0800

    Update code_segments.cpp

--- code_segments.cpp
+++ code_segments.cpp
@@ -1,3 +1,24 @@
+void checkCompile(GLuint shaderId, int shaderType) {
+    int success;
+    char log[512];
+    if (shaderType == 0) {
+        glGetProgramiv(shaderId, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shaderId, sizeof log, NULL, log);
+            std::cout << "ERROR: LINK\n" << log << std::endl;
+        }
+    } else {
+        char *type;
+        if (shaderType == 1)    type = "VERTEX";
+        else                    type = "FRAGMENT";
+        glGetShaderiv(shaderId, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(shaderId, sizeof log, NULL, log);
+            std::cout << "ERROR: " << type << " SHADER\n" << log << std::endl;
+        }
+    }
+}
+
 void drawLine(float *p, float *q) {
     // Vertex shader
     GLuint vshader = glCreateShader(GL_VERTEX_SHADER);
@@ -8,6 +29,7 @@ void drawLine(float *p, float *q) {
         "}\n";
     glShaderSource(vshader, 1, &vsource, NULL);
     glCompileShader(vshader);
+    checkCompile(vshader, 1);
     // Fragment shader
     GLuint fshader = glCreateShader(GL_FRAGMENT_SHADER);
     const char *fsource = "#version 430 core\n"
@@ -17,11 +39,13 @@ void drawLine(float *p, float *q) {
         "}\n";
     glShaderSource(fshader, 1, &fsource, NULL);
     glCompileShader(fshader);
+    checkCompile(fshader, 2);
     // Shader program
     GLuint program = glCreateProgram();
     glAttachShader(program, vshader);
     glAttachShader(program, fshader);
     glLinkProgram(program);
+    checkCompile(program, 0);
     // VAO
     GLuint vao;
     glGenVertexArrays(1, &vao);
